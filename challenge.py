562740e7d29eb0a29565eb3746867eb6150bfc39
Lewis Gaul <Lewis@scphillips.com>
2016-12-29 17:03:31 +0000

    Creation of challenge

---
+++ challenge.py
@@ -0,0 +1,15 @@
+# WEEK 1 CHALLENGE
+
+# 
+
+# Notice you can write single-line comments with the '#' symbol.
+# This text will not be executed by the interpreter.
+
+# Blank lines can be used wherever you like to make your code easier to read.
+# Try not to let your code exceed 80 characters in width (see 'Col: ##' in the
+# corner).
+
+
+var1 = 3
+var2 = 5
+var3 = var1 * var2 - 1
