ac82308a8b8d3370644a1361095cb70969b5a8ff
Lewis Gaul <Lewis@scphillips.com>
2017-01-28 22:44:32 +0000

    Create challenge3

---
+++ challenge3.py
@@ -0,0 +1,14 @@
+"github.com/lewisgaul/python-tutorial"
+# WEEK 3 CHALLENGE - functions
+
+# Work out how the code below works (use print statements or display variables
+# in the shell), add in some comments and make some improvements by using better
+# variable names and clearer code where you can.
+
+
+
+
+
+
+
+
