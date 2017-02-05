6a33127b85376e2fe164563c20855453a680c86e
Lewis Gaul <Lewis@scphillips.com>
2017-02-05 17:56:11 +0000

    Add template for challenge4

---
+++ challenge4/main.py
@@ -0,0 +1,6 @@
+"github.com/lewisgaul/python-tutorial"
+# WEEK 4 CHALLENGE - functions and imports
+
+# Work out how the code works (use print statements, try running functions
+# in the shell), add in some comments to help yourself understand.
+
