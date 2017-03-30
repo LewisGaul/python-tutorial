dfe7c2c61c9028e7795527aaf782986f74e873c9
Lewis Gaul <Lewis@scphillips.com>
2017-03-30 16:54:55 +0100

    Create week 8 materials

--- challenge8.py
+++ challenge8.py
@@ -1,4 +1,12 @@
 "github.com/lewisgaul/python-tutorial"
 # WEEK 8 CHALLENGE
 
+# Pick one of the following and see if you can do it from scratch:
+#   Create a GUI game, e.g. buttons which light up in different colours for
+#   short periods of time, with the aim being to click buttons that flash
+#   green but not when they flash red (or something like that...).
 
+#   First Google 'persistence of a number'. Create a dictionary with the
+#   persistance as the key, and a list of numbers with that persistance as
+#   the corresponding value. Once you have done this I can show you how to plot
+#   graphs in Python.
